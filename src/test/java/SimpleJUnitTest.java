cdc849d1eec0eab4ed8d3babfe3d5ab7f592b030
nefiodova <an.nefd@yandex.com>
2025-06-22 16:42:58 +0300

    added JUnit 5

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class SimpleJUnitTest {
+
+    int result;
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("### beforeEach()");
+        result = getResult();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("###  afterEach()");
+        result = 0;
+    }
+
+    @Test
+    void firstTest() {
+        int result = getResult();
+        System.out.println("###   firstTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void secondTest() {
+        int result = getResult();
+        System.out.println("###   secondTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        int result = getResult();
+        System.out.println("###   thirdTest()");
+        Assertions.assertTrue(result > 2);
+    }
+
+    private int getResult() {
+        return 3;
+    }
+}
